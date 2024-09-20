7bf3a28e7ed21d19ec4390319f24ae9962ad9c9b
EliudQ <eliuddanielagq22@gmail.com>
2024-09-20 08:04:17 -0500

    Se crea un nuevo archivo y se edita estilos

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@ document.getElementById('startButton').addEventListener('click', function() {
         alert('Archivo seleccionado: ' + fileInput.files[0].name);
         // Aquí puedes agregar la lógica para procesar el archivo
     } else {
+        event.preventDefault();
         alert('Por favor, selecciona un archivo CSV antes de comenzar.');
     }
 });
